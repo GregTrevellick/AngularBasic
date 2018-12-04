bd27aaebc56e270ac42ff426917fc938653ea222
Greg Trevellick <gtrevellick@googlemail.com>
2018-12-04 20:59:41 +0000

    .

--- vsixTemplatePackSideWaffle/Properties/AssemblyInfo.cs
+++ vsixTemplatePackSideWaffle/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vsixTemplatePackSideWaffleProject")]
+[assembly: AssemblyTitle("vsixTemplatePackSideWaffle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("vsixTemplatePackSideWaffleProject")]
+[assembly: AssemblyProduct("vsixTemplatePackSideWaffle")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
